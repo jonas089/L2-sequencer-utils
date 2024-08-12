f9f7532fa3274684532a4b6acc9b94fd65b4af4f
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-08-12 19:26:14 +0530

    Update types.rs

--- zk-logic/src/types.rs
+++ zk-logic/src/types.rs
@@ -6,14 +6,14 @@ pub type Nonce = Vec<u8>;
 // serialized Seed
 pub type Seed = Vec<u8>;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CircuitInputs {
     pub public_key: PublicKey,
     pub nonce: Nonce,
     pub seed: Seed,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CircuitOutputs {
     pub public_key: PublicKey,
     pub nonce: Nonce,
